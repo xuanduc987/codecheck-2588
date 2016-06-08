73fc0f8ddc468babfa0677553bcdfaae14c4d717
Duc Nghiem Xuan <xuanduc987@gmail.com>
2016-06-08 20:23:18 +0700

    Implement methdo prime? and A

--- app/program.rb
+++ app/program.rb
@@ -1,10 +1,14 @@
 def A(x, y)
-  # 関数Aを作成する．
-  # 返り値は整数
+  raise ArgumentError, 'x and y must be >= 0' if x < 0 || y < 0
+  return y + 1 if x == 0
+  return A(x - 1, 1) if y == 0
+  return A(x - 1, A(x, y - 1))
 end
 
 def prime?(n)
-  # 素数を判定する関数 prime? を作成する．
-  # 返り値は true or false
+  return false if !(n.is_a? Integer) || n < 2
+  (2..Math.sqrt(n)).each do |i|
+    return false if n % i == 0
+  end
   return true
 end
